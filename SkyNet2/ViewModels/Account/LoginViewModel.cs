e24b578e9b78edff6abfddf1e29a5d6f474a9c9a
v-king777 <v.kolpashchikov@gmail.com>
2021-12-09 23:28:14 +0300

    09.12.21.23.28

--- SkyNet2/ViewModels/Account/LoginViewModel.cs
+++ SkyNet2/ViewModels/Account/LoginViewModel.cs
@@ -7,15 +7,15 @@ public class LoginViewModel
     [Required(ErrorMessage = "Поле обязательно для заполнения")]
     [EmailAddress]
     [Display(Name = "Email", Prompt = "Введите email")]
-    public string? Email { get; set; }
+    public string Email { get; set; }
 
     [Required(ErrorMessage = "Поле обязательно для заполнения")]
     [DataType(DataType.Password)]
     [Display(Name = "Пароль", Prompt = "Введите пароль")]
-    public string? Password { get; set; }
+    public string Password { get; set; }
 
     [Display(Name = "Запомнить?")]
     public bool RememberMe { get; set; }
 
-    public string? ReturnUrl { get; set; }
+    public string ReturnUrl { get; set; }
 }
